42c831954df4b12f2e50707dcb74214d531c54dd
Hugo <hugonotario29@gmail.com>
2021-10-22 15:54:19 +0200

    reorganizacion

---
+++ Proyectos intellij/practica1/src/Exercici_5.java
@@ -0,0 +1,29 @@
+/*
+Este programa calcula y muestra por pantalla los primeros 40 numeros primos. 197
+ */
+
+public class Exercici_5 {
+    public static void main(String[] args) {
+        // Descripcion del programa
+        System.out.println("Este programa calcula y muestra por pantalla los " +
+                "primeros 40 numeros primos.");
+        System.out.println();
+
+        // Declaración de variables.
+        int ciclo = 1;
+        int numero = 1;
+        boolean esPrimo;
+
+        while (ciclo <= 40) {
+            if (numero <= 2) {
+                esPrimo = true;
+            } else {
+                while () {
+
+                }
+            }
+
+        }
+
+    }
+}
