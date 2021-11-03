4230d2400c0dbbed74b4c0c1081663b918cfdb88
Your Name <you@example.com>
2021-11-03 09:15:33 +0100

    asdf

---
+++ Proyectos intellij/Exercicis_Grafics/src/Exercici_5.java
@@ -0,0 +1,15 @@
+public class Exercici_5 {
+    // funcion cuadrado
+
+    public static void main(String[] args) {
+        int lado = 30;
+        int espacio = 15;
+
+        for (int i = 0; i < ; i++) {
+            cuadrado(lado);
+            sigCuadr();
+            lado = lado + (espacio*2);
+        }
+
+    }
+}
