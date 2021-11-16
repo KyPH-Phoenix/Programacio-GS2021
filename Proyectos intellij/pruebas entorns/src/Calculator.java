e60bd1e773060782992ffe9592d7322a5d4ef473
Hugo Notario <hugonotario29@gmail.com>
2021-11-16 20:17:13 +0100

    calculator aaaa

---
+++ Proyectos intellij/pruebas entorns/src/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+    static int add (int x, int y) {
+        return x + y;
+    }
+
+    static int substract (int x, int y) {
+        return x - y;
+    }
+
+    static int product (int x, int y) {º
+        return x * y;
+    }
+
+    static int divide (int x, int y) {
+        return x / y;
+    }
+
+    static int remainder (int x, int y) {
+        return x % y;
+    }
+
+    public static void main(String[] args) {
+    }
+}
