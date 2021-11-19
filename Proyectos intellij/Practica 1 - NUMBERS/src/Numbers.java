43c6339763eced6539d3830dd2341c3fdcc561cb
Hugo Notario <hugonotario29@gmail.com>
2021-11-19 19:14:23 +0100

    numbers inicial

---
+++ Proyectos intellij/Practica 1 - NUMBERS/src/Numbers.java
@@ -0,0 +1,78 @@
+public class Numbers {
+    public static String say(long n) {
+        String numero = "";
+        switch ((int) n) {
+            case 0:
+                numero = "zero";
+                break;
+            case 1:
+                numero = "one";
+                break;
+            case 2:
+                numero = "two";
+                break;
+            case 3:
+                numero = "three";
+                break;
+            case 4:
+                numero = "four";
+                break;
+            case 5:
+                numero = "five";
+                break;
+            case 6:
+                numero = "six";
+                break;
+            case 7:
+                numero = "seven";
+                break;
+            case 8:
+                numero = "eight";
+                break;
+            case 9:
+                numero = "nine";
+                break;
+            case 10:
+                numero = "ten";
+                break;
+            case 11:
+                numero = "eleven";
+                break;
+            case 12:
+                numero = "twelve";
+                break;
+            case 13:
+                numero = "thirteen";
+                break;
+            case 14:
+                numero = "fourteen";
+                break;
+            case 15:
+                numero = "fifteen";
+                break;
+            case 16:
+                numero = "sixteen";
+                break;
+            case 17:
+                numero = "seventeen";
+                break;
+            case 18:
+                numero = "eighteen";
+                break;
+            case 19:
+                numero = "nineteen";
+        }
+        return numero;
+    }
+
+    public static long words(String s) {
+        return 0;
+    }
+
+    private static String primeraMayus (String numero) {
+        String result = "";
+        result += (char) (numero.charAt(0) - 32);
+        numero.charAt(0) =
+        return numero;
+    }
+}
