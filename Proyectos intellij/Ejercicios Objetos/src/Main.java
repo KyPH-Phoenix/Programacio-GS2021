000fba5a3c0d336202ffbb13bf1f0ceae10d8216
Your Name <you@example.com>
2022-01-27 21:31:13 +0100

    Peix (menos count)

--- Proyectos intellij/Ejercicios Objetos/src/Main.java
+++ Proyectos intellij/Ejercicios Objetos/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        QuadratMagic quad1 = new QuadratMagic();
-        quad1.imprimeix(5);
+        Peix p1 = new Peix("Manolo");
+        Peix p2 = new Peix(p1);
+        Peix p3 = new Peix(p2);
+
+        System.out.printf("%d, %d, %d", p1.count, p2.count, p3.count);
     }
 }
--- Proyectos intellij/Ejercicios Objetos/src/Peix.java
+++ Proyectos intellij/Ejercicios Objetos/src/Peix.java
@@ -1,3 +1,26 @@
 public class Peix {
+    String nom;
 
+    int count = 1;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    Peix(String nom) {
+        setNom(nom);
+    }
+
+    Peix(Peix p) {
+        this.count = p.count + 1;
+        this.nom = p.getNom();
+    }
+
+    public boolean iguals(Peix p) {
+        return this.equals(p);
+    }
 }
