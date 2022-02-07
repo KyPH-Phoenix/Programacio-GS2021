c7958ddac1e3a6183d69db0f876b30ca88c31b09
Hugo Notario <hugonotario29@gmail.com>
2022-02-07 20:14:13 +0100

    Polynomial test 2 correcto

--- Proyectos intellij/Polynomial/src/Main.java
+++ Proyectos intellij/Polynomial/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Polynomial p1 = new Polynomial(new float[]{-1, 5, 0, 0, 6, 3});
+        Polynomial p2 = new Polynomial("3x + 5 + 2x^2");
 
-        System.out.println(p1.toString());
+        System.out.println(p2.toString());
     }
 }
--- Proyectos intellij/Polynomial/src/Polynomial.java
+++ Proyectos intellij/Polynomial/src/Polynomial.java
@@ -15,6 +15,56 @@ public class Polynomial {
 
     // Constructor a partir d'un string
     public Polynomial(String s) {
+        s = s.replace("+ ", "");
+        s = s.replace("- ", "-");
+
+        String[] variables = s.split(" ");
+
+        int grauMax = grauTermes(variables, null);
+
+        float[] polinomi = construeixArray(grauMax);
+        grauTermes(variables, polinomi);
+
+        this.poliArray = polinomi;
+    }
+
+    int grauTermes (String[] variables, float[] polinomi){
+        int grauMax = 0;
+
+        for (int i = 0; i < variables.length; i++) {
+            String posicio = variables[i];
+            posicio = posicio.replace("^", "");
+
+            if (posicio.charAt(posicio.length()-1) == 'x') posicio += "1";
+
+            if (posicio.charAt(0) == 'x') posicio = "1" + posicio;
+            if (posicio.length() > 1 && posicio.substring(0, 2).equals("-x")) posicio = "-1" + posicio.substring(1);
+
+            String[] elemento = posicio.split("x");
+
+            if (elemento.length > 1) {
+                grauMax = Math.max(Integer.parseInt(elemento[1]), grauMax);
+            }
+
+            if (polinomi != null) {
+                int valor = Integer.parseInt(elemento[0]);
+                int grau = (elemento.length > 1) ? Integer.parseInt(elemento[1]) : 0;
+
+                polinomi[polinomi.length - grau - 1] += valor;
+            }
+        }
+
+        return grauMax;
+    }
+
+    float[] construeixArray(int grauMax) {
+        float[] array = new float[grauMax + 1];
+
+        for (int i = 0; i < grauMax + 1; i++) {
+            array[i] = 0;
+        }
+
+        return array;
     }
 
     // Suma el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
