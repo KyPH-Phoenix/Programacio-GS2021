3e92cdd069f724c8e90b468eb0dbd812a7ec6d61
Hugo Notario <hugonotario29@gmail.com>
2022-05-04 20:15:14 +0200

    barcode empezadoa asdfasdfsadf

---
+++ Proyectos intellij/Ejercicios Listas/src/Containers2/Exercici8/Main.java
@@ -0,0 +1,20 @@
+package Containers2.Exercici8;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> l1 = new ArrayList<Integer>();
+        fill(l1);
+        List<Integer> l2 = new LinkedList<Integer>();
+        fill(l2);
+        test(l1);
+        test(l2);
+    }
+
+    private static void fill(List<Integer> l1) {
+
+    }
+}
