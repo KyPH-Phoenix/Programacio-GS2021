96fb051f2f6c05b19cf026d22c868fa32c7918de
Hugo Notario <hugonotario29@gmail.com>
2022-05-24 16:56:58 +0200

    ME CAGO EN TU PUTA MADRE LOCO QUE NO QUIERO ESTA PUTA MIERDA A VER SI TE ENTERAS

--- Proyectos intellij/Barcode/src/Main.java
+++ Proyectos intellij/Barcode/src/Main.java
@@ -3,10 +3,12 @@ import static org.junit.Assert.*;
 
 public class Main {
     public static void main(String[] args) {
-        Image i1 = new Image(UtilTests.getImageAsString("code11_0123452.ppm"));
+        Code11.decode("█ ██  █ █ █ ██ █ ██  █");
 
+        Code11.decode("█ ███  ██ █ █ █ █ ███ █ ███   █");
 
-        System.out.println(i1);
+        Code11.decodeImage(UtilTests.getImageAsString("code11_446688120.ppm"));
+
 //        System.out.println(Code11.decodeImage(UtilTests.getImageAsString("code11_0123452.ppm")));
 //        Code11.decodeImage(UtilTests.getImageAsString("code11_0123-4567.ppm"));
 
